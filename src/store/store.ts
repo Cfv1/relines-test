95c52fbc68b952c4f2e5418d06d36f37f944595e
Vlad <kzvlad3@gmail.com>
2023-05-16 12:07:09 +0700

    logger

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,12 @@
 import {configureStore} from '@reduxjs/toolkit';
 import UsersSlice from './reducers/users/usersSlice';
+import {logger} from './helpers/logger';
 
 export const store = configureStore({
   reducer: {
     usersReducer: UsersSlice.reducer,
-  }
+  },
+  middleware: (getDefault) => getDefault().concat(logger)
 });
 
 export type AppDispatch = typeof store.dispatch;
